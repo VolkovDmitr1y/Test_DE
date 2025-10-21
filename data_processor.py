80ac9bdc5559cfcb217fe74c85f946d81f54d7a1
Dima <volkovdvitrij288@gmail.com>
2025-10-22 00:56:55 +0300

    Добавил библиотеки PySpark

--- data_processor.py
+++ data_processor.py
@@ -1,2 +1,23 @@
 import pandas as pd 
 import numpy as np
+
+
+# Добавление библиотек PySpark
+
+# Импорт необходимых модулей
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+
+# Создание Spark сессии
+spark = SparkSession.builder \
+    .appName("PizzaRestaurantAnalysis") \
+    .config("spark.sql.adaptive.enabled", "true") \
+    .getOrCreate()
+
+# Проверка
+spark
+
+
+
